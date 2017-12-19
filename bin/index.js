b37d1b43a8f19302bc1fcbf94d7475a3b52721b5
TerrySu <2684759855@qq.com>
2017-12-19 10:46:38 +0800

    [Test-Jasmine] Add

---
+++ bin/index.js
@@ -0,0 +1,13 @@
+// ****** Main step - Get paths of pages to develop ******
+
+
+
+// ****** Main step - Run webpack ******
+
+
+
+// ****** Main step - Copy files ******
+
+
+
+// ****** Main step - Server output ******
